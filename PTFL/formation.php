bdc6a988f304f39dbd7524074081158510a59399
RyadBAAZIZ <166405738+Bebeto92@users.noreply.github.com>
2024-04-12 01:26:10 +0200

    Add files via upload

---
+++ PTFL/formation.php
@@ -0,0 +1,127 @@
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Portfolio BTS SIO SLAM qui retrace les réalisations pendant les 2 ans de BTS">
+        <title>Portfolio DALLAY Cassandre</title>
+
+        <!--Liaison au fichier css de Bootstrap…-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+        <!--/Liaison au fichier css de Bootstrap…-->
+
+        <!-- Font Gilda display + DM Sans-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Gilda+Display&display=swap" rel="stylesheet">
+        <!-- /Font Gilda display + DM Sans-->
+
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" type="image/x-icon" href="assests/images/logo_cass.png" class="img-fluid">
+        <link rel="apple-touch-icon" href="assests/images/logo_cass.png">
+    </head>
+    <body>
+        <div class="row sm-1">
+            <nav class="navbar navbar-expand-xl navbar-light navbar-custom col-md-2">
+                <div class="container-fluid col-12 offset-md-1 col-md-12 col-lg-12 col-xl-12">
+                    <a href="index.php">
+                    <img src="assests/images/logo_cass.png" alt="Logo DALLAY Cassandre" class="logo img-fluid"/>
+                    <br>
+                    </a>
+                    <!--gestion du menu pour les support mobiles-->
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mobile">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <!--fin gestion menu support mobile-->
+                    <!-- Menu déroulant -->
+                </div>
+                <div class="collapse navbar-collapse" id="mobile">
+                    <nav>
+                        <ul>
+                            <li><a href="index.php">Accueil</a></li>
+                            <li><a href="formation.php">Formations</a></li>
+                            <li><a href="ap.php">Réalisations</a></li>
+                            <li><a href="stage.php">Expériences</a></li>
+                            <li><a href="veille.php">Veille technologique</a></li>
+                            <li><a href="certif.php">Certifications</a></li>
+                        </ul>
+                        <ul>
+                            <li class="nav_icon">
+                                <a href="https://www.linkedin.com/in/cassandre-dallay-14868a1b1" target="blank" title="LinkedIn"><svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi-linkedin icon" viewBox="0 0 16 16" >
+                                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                                </svg>
+                                </a> 
+
+                                <a ata-bs-placement="bottom"  data-bs-toggle="tooltip" title="CV" href="assests/documentation/cv/CV.pdf" target="blank"  d><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-file-earmark-person icon" viewBox="0 0 16 16">
+                                <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm2 5.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-.245S4 12 8 12s5 1.755 5 1.755z"/></svg>
+                                </a>
+
+
+                            </li>
+
+                        </ul>
+                    </nav>
+                </div>
+            </nav>
+        </div>
+<div class="row-1">
+    <div class="content col-5">
+        <h2>Formations</h2>
+    </div>
+</div>
+<div class="row-1">
+    <div class="col-11 col-sm-3 book">
+        <img src="assests/images/formation/book_education.svg" alt="book_image" class="book">
+    </div>
+</div>
+<div class="row-2">
+    <div class="content md-5">
+        <section class="dm">
+        <ul class="timeline">
+          <li class="timeline-item mb-4">
+            <h5 class="fw">2021 - 2023</h5>
+            <p class="text">
+              BTS <span class="color">S</span>ervices 
+              <span class="color">I</span>nformatiques aux 
+              <span class="color">O</span>rganisations (<span class="color">SIO</span>)<br>
+              option <span class="color">S</span>olutions <span class="color">L</span>ogicielles et <span class="color">A</span>pplications Métiers (<span class="color">SLAM</span>)<br>
+              Au <a class="lycee" target="_blank" href="https://lycee-paul-louis-courier.fr/2022/06/22/bts-sio-services-informatiques-aux-organisations/">lycée Paul-Louis Courier</a> à Tours
+            </p>
+          </li>
+          <li class="timeline-item mb-4">
+            <h5 class="fw">2021</h5>
+            <p class="text">
+              Bac Professionnel <span class="color">S</span>ystème <span class="color">N</span>umérique (<span class="color">SN</span>)<br>
+              option <span class="color">R</span>éseaux <span class="color">I</span>nformatiques et <span class="color">S</span>ystèmes <span class="color">C</span>ommunicants (<span class="color">RISC</span>)<br>
+              Au <a class="lycee" target="_blank" href="https://www.lp-becquerel.eu">lycée Henri Becquerel</a> à Tours<br>
+              Obtenu avec la mention Très Bien
+            </p>
+          </li>
+          <li class="timeline-item mb-4">
+            <h5 class="fw">2020</h5>
+            <p class="text">
+              BEP <span class="color">S</span>ystème <span class="color">N</span>umérique (<span class="color">SN</span>)<br>
+              option <span class="color">R</span>éseaux <span class="color">I</span>nformatiques et <span class="color">S</span>ystèmes <span class="color">C</span>ommunicants (<span class="color">RISC</span>)<br>
+              Au <a class="lycee" target="_blank" href="https://www.lp-becquerel.eu">lycée Henri Becquerel</a> à Tours<br>
+            </p>
+          </li>
+        </ul>
+      </section>
+
+      
+</div>
+</body>
+<footer class="fixed-bottom">
+    <ul class="foot">
+        <li><a href="mention.php" class="foot_link">Mentions légales</a></li>
+        <li><a href="propos.php" class="foot_link">À propos</a></li>
+        <li class="carbon col-xs-1"><span id="wcb" class="carbonbadge">
+            <script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js" defer></script></span>
+        </li>
+    </ul>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+</script>
+</html>
